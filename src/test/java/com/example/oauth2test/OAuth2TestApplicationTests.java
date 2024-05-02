c2e2e7acf618efb408301be438d047897c4a0f40
yuuzinn <s110528@nate.com>
2024-05-02 18:12:21 +0900

    Initial commit

---
+++ src/test/java/com/example/oauth2test/OAuth2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
